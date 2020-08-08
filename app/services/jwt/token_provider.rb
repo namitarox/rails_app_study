df1d56be47fb03e23cbcd88fd63d38122a0491c9
浪岡大貴 <namioka@pixel-hearts.co.jp>
2020-08-08 19:22:33 +0900

    update ログイン認証の作成

---
+++ app/services/jwt/token_provider.rb
@@ -0,0 +1,14 @@
+module Jwt::TokenProvider
+  extend self
+
+  def call(payload)
+    print(payload)
+    issue_token(payload)
+  end
+
+  private
+
+  def issue_token(payload)
+    JWT.encode(payload, Rails.application.credentials.secret_key_base)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users, only: %i[create]
+    resource :session, only: %i[create]
   end
 end
