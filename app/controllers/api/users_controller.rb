352cbe0dbc4aef916dbd6ea6c1737cd9490e955a
浪岡大貴 <namioka@pixel-hearts.co.jp>
2020-08-08 19:32:14 +0900

    fix serializerの追加

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     user.save!
-    render json: user
+    render json: user, serializer: UserSerializer
   end
 
   private
