a8fe3c4e49dfca2d1d84e5d2eb64c959ca39258e
浪岡大貴 <namioka@pixel-hearts.co.jp>
2020-08-12 00:17:21 +0900

    update hello worldの表示 vue-loader vuexのインストールまで

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,20 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import Vue from 'vue'
+import App from '../app.vue'
+document.addEventListener('DOMContentLoaded', () => {
+    const el = document.body.appendChild(document.createElement('hello'))
+    const app = new Vue({
+        el,
+        render: h => h(App)
+    })
 
+    console.log(app)
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
