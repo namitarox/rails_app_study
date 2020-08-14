9905847e081ea608eb4580cf9722d922573dac79
浪岡大貴 <namioka@pixel-hearts.co.jp>
2020-08-14 21:19:06 +0900

    update signupのページを作成

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
-import PageTimeline from '@/pages/PageTimeline'
+import PageUserRegister from '@/pages/PageUserRegister'
 import PageUserLogin from '@/pages/PageUserLogin'
+import PageTimeline from '@/pages/PageTimeline'
 const router = new VueRouter({
     routes: [
-        { path: '/login', component: PageUserLogin, name: 'timeline' },
-        { path: '/', component: PageTimeline, name: 'login' }
+        { path: '/signup', component: PageUserRegister, name: 'user-register' },
+        { path: '/login', component: PageUserLogin, name: 'user-login' },
+        { path: '/', component: PageTimeline, name: 'timeline' },
     ]
 });
 
