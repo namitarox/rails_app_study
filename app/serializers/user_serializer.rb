c7fea9d150227c1d2b6ceb132a139dfc4a093fc0
浪岡大貴 <namioka@pixel-hearts.co.jp>
2020-08-25 02:26:16 +0900

    fix 関数名の変更

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :introduction, :avatar
-  def avatar
+  attributes :id, :name, :email, :introduction, :avatar_url
+  def avatar_url
     return nil unless object.avatar.attached?
 
     Rails.application.routes.url_helpers.rails_blob_path(object.avatar, only_path: true)
