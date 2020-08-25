4e1b7e00b7dd9b50bb2e53b327371bb3adfbd49c
浪岡大貴 <namioka@pixel-hearts.co.jp>
2020-08-26 01:24:32 +0900

    update ルーターの設定

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -5,13 +5,17 @@ import PageUserRegister from '@/pages/PageUserRegister'
 import PageUserLogin from '@/pages/PageUserLogin'
 import PageTimeline from '@/pages/PageTimeline'
 import PageMicropostDetail from '@/pages/PageMicropostDetail'
+import PageProfile from '@/pages/PageProfile'
 const router = new VueRouter({
     routes: [
         { path: '/signup', component: PageUserRegister, name: 'user-register' },
         { path: '/login', component: PageUserLogin, name: 'user-login' },
         { path: '/', component: PageTimeline, name: 'timeline' },
         { path: '/microposts', component: PageTimeline },
-        { path: '/microposts/:id', component: PageMicropostDetail, name: 'micropost-detail' },]
+        { path: '/microposts/:id', component: PageMicropostDetail, name: 'micropost-detail' },
+        { path: '/profile', component: PageProfile, name: 'user-profile' },
+        { path: '/users/:id', component: PageProfile, name: 'user-profile' },
+    ]
 });
 
 export default router;
