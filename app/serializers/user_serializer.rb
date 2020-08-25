ed25f784dfc345ca6b5bc36273a3d18bc91be702
浪岡大貴 <namioka@pixel-hearts.co.jp>
2020-08-25 17:27:54 +0900

    update serializerの編集

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :introduction, :avatar_url
-  def avatar_url
-    return nil unless object.avatar.attached?
 
-    Rails.application.routes.url_helpers.rails_blob_path(object.avatar, only_path: true)
+  def avatar_url
+    if object.avatar.attached?
+      Rails.application.routes.url_helpers.rails_blob_path(object.avatar, only_path: true)
+    else
+      'https://placehold.jp/300x300.png'
+    end
   end
 end
