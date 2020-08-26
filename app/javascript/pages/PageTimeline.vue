d4436fe057eb1f6a534254b3fccb8a7c30c151b4
浪岡大貴 <namioka@pixel-hearts.co.jp>
2020-08-26 23:36:00 +0900

    update paging対応

--- app/javascript/pages/PageTimeline.vue
+++ app/javascript/pages/PageTimeline.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row>
       <v-col>
-        <div>
+        <div class="pb-10">
           <div
             class="mb-5"
             v-if="isLogin"
@@ -12,6 +12,7 @@
           <timeline-list
             v-if="isExistMicroposts"
             :microposts="microposts"
+            class="mb-5"
           ></timeline-list>
           <div
             class="text-center"
@@ -19,6 +20,16 @@
           >
             一件もありません
           </div>
+          <template v-if="pagingMeta">
+            <div class="text-center">
+              <v-pagination
+                color="indigo"
+                v-model="pagingMeta.current_page"
+                :length="pagingMeta.total_pages"
+                @input="paging"
+              ></v-pagination>
+            </div>
+          </template>
         </div>
       </v-col>
     </v-row>
@@ -49,8 +60,11 @@ export default {
   },
   methods: {
     async fetchMicroposts() {
-      const res = await axios.get(`/api/microposts`);
+      const res = await axios.get(`/api/microposts`, {
+        params: { page: this.currentPage },
+      });
       this.microposts = res.data.microposts;
+      this.pagingMeta = res.data.meta;
     },
     async createMicropost(micropostContent) {
       const micropostParams = {
@@ -60,6 +74,12 @@ export default {
       };
       const res = await axios.post(`/api/microposts`, micropostParams);
       this.microposts = [...[res.data.micropost], ...this.microposts];
+      this.paging(1);
+    },
+    paging(page) {
+      this.currentPage = page;
+      this.fetchMicroposts();
+      this.$vuetify.goTo(0);
     },
   },
 };
